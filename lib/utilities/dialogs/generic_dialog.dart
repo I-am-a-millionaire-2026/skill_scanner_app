ab31aa11b032b1369958be3426f88adf6bb8c846
Sara Kasraie <kasraie.sara@gmail.com>
2026-01-12 12:38:12 +1100

    step15 notes stream and list view completed

--- lib/utilities/dialogs/generic_dialog.dart
+++ lib/utilities/dialogs/generic_dialog.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/material.dart'; // <--- این خط تمام ارورهای قرمز تصویر را حل می‌کند
-
-typedef DialogOptionBuilder<T> = Map<String, T?> Function();
+import 'package:flutter/material.dart';
 
 Future<T?> showGenericDialog<T>({
   required BuildContext context,
   required String title,
   required String content,
-  required DialogOptionBuilder<T> optionsBuilder,
+  required Map<String, T?> Function() optionsBuilder,
 }) {
   final options = optionsBuilder();
   return showDialog<T>(
@@ -16,15 +14,9 @@ Future<T?> showGenericDialog<T>({
         title: Text(title),
         content: Text(content),
         actions: options.keys.map((optionTitle) {
-          final T? value = options[optionTitle];
           return TextButton(
-            // <--- حالا TextButton شناخته می‌شود
             onPressed: () {
-              if (value != null) {
-                Navigator.of(context).pop(value);
-              } else {
-                Navigator.of(context).pop();
-              }
+              Navigator.of(context).pop(options[optionTitle]);
             },
             child: Text(optionTitle),
           );
